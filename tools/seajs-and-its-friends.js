efcd17b37fc3999c7638d4c35aee01d27f34f920
lifesinger <lifesinger@gmail.com>
2012-05-02 16:38:16 +0800

    add seajs-and-its-friends.js

---
+++ tools/seajs-and-its-friends.js
@@ -0,0 +1,24 @@
+
+// load sea.js
+(function(m, o, d, u, l, a, r) {
+    if(m[d]) return;
+    function f(n, t) { return function() { r.push(n, arguments); return t; } }
+    m[d] = a = { args: (r = []), config: f(0, a), use: f(1, a) };
+    m.define = f(2);
+    u = o.createElement('script');
+    u.id = d + 'node';
+    u.src = '../../../dist/seajs/1.1.0/sea.js';
+    l = o.getElementsByTagName('head')[0];
+    l.insertBefore(u, l.firstChild);
+})(window, document, 'seajs');
+
+
+// and its friends
+seajs.config({
+    alias: {
+        'json': 'json/1.0.1/json',
+        'underscore': 'underscore/1.3.3/underscore',
+        'jquery': 'jquery/1.7.2/jquery'
+    },
+    preload: [this.JSON ? '' : 'json', 'plugin-text']
+});
