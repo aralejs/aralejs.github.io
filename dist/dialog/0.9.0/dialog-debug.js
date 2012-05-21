09eb029ac52cd1c1bf794d7c8c9ba501ab6cbeda
popomore <sakura9515@gmail.com>
2012-05-21 22:08:56 +0800

    update dist overlay 0.9.1

---
+++ dist/dialog/0.9.0/dialog-debug.js
@@ -0,0 +1,73 @@
+define("#dialog/0.9.0/dialog-debug", ["$","position","overlay","mask","events"], function(require, exports, module) {
+
+    var $ = require('$'),
+        Position = require('position'),
+        Overlay = require('overlay'),
+        mask = require('mask'),
+        Events = require('events');
+
+
+    var Dialog = Overlay.extend({
+
+        options: {
+            trigger: null,
+            triggerType: 'click',
+            comfirmElement: null,
+            cancelElement: null,
+            closeElement: null,
+            hasMask: false
+        },
+
+        parseElement: function() {
+            Dialog.superclass.parseElement.call(this);
+            var options = this.options;
+            options.trigger = $(options.trigger);
+            options.comfirmElement = this.$(options.comfirmElement);
+            options.cancelElement = this.$(options.cancelElement);
+            options.closeElement = this.$(options.closeElement);
+        },
+
+        delegateEvents: function() {
+            var options = this.options,
+                that = this;
+            
+            // 绑定触发对话框出现的事件
+            options.trigger.bind(options.triggerType, function() {
+                that.options.hasMask && mask.show();
+                that.render().show();
+            });
+
+            // 点击确定元素
+            var comfirmHandler = function() {
+                that.onComfirm();
+                that.hide();
+            };
+            options.comfirmElement.bind('click', comfirmHandler);
+
+            // 点击取消或关闭元素
+            var closeHandler = function() {
+                that.onClose();
+                that.options.hasMask && mask.hide();
+                that.hide();
+            };
+            options.cancelElement.bind('click', closeHandler);
+            options.closeElement.bind('click', closeHandler);
+
+            // 绑定确定和关闭事件到 dom 元素上，以供全局调用
+            Events.mixTo(this.element[0]);
+            this.element[0].on('comfirm', comfirmHandler);
+            this.element[0].on('close cancel', closeHandler);
+        },
+
+        // 点击确定时触发的功能，供覆盖
+        onComfirm: function() {},
+
+        // 点击取消或关闭时触发的功能，供覆盖
+        onClose: function() {}
+
+    });
+
+    module.exports = Dialog;
+
+});
+
---
+++ dist/dropdown/0.9.0/dropdown-debug.js
@@ -0,0 +1,77 @@
+// May the Source be with you
+// 愿源码与你同在
+
+define("#dropdown/0.9.0/dropdown-debug", ["jquery","base","overlay"], function(require, exports, module) {
+    var $ = require('jquery');
+    var Base = require('base');
+    var Overlay = require('overlay');
+    var MyOverlay = Overlay.extend({
+        getElem: function() {
+            return this.srcNode;
+        },
+        toggle: function() {
+            $(this.srcNode).toggle();
+        }
+    });
+    var Dropdown = Base.extend({
+        options: {
+            trigger: '', // required
+            triggerType: 'hover', // click, hover
+            target: '.ui-dropdown',
+            position: 'left', // center, right
+            container: 'ui-content',
+            animateType: '', // height, width, opacity
+            width: 'auto',
+            height: 'auto',
+            speed: '200',
+            x: 0,
+            y: 0,
+            z: 999 // z-index
+        },
+
+        initOverlay: function() {
+            var opt = this.options;
+            var $trigger = $(opt.trigger);
+            var $target = $(opt.target);
+            var overlay = new MyOverlay({
+                //srcNode: $trigger[0],
+                template: '<div class="ui-dropdown"></div>',
+                content : $target.html(),
+                parentNode : $trigger.parent()[0],
+                width : opt.width,
+                height : opt.heigth,
+                zIndex: opt.z,
+                baseObject : {
+                    element: $trigger[0],
+                    x : 0,
+                    y : $trigger.height(),
+                }
+            });
+            return overlay;
+        },
+
+        initialize: function(options) {
+            if (!options || !options.trigger) {
+                throw 'options/trigger missing';
+            }
+            this.setOptions(options);
+            var that = this;
+            var opt = this.options;
+            var $target;
+            $(opt.trigger).on(opt.triggerType, function() {
+                if (!$target) {
+                    $target = that.initOverlay();
+                }
+                $target.render().show();
+            });
+            $(opt.trigger).parent().on('hover', $.noop, function() {
+                if ($target) {
+                    $target.hide();
+                }
+            });
+        }
+    });
+
+    module.exports = Dropdown;
+
+});
