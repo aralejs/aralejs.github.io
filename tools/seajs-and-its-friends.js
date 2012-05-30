efaaa4d9ede3ed318b9fffed7b78f5be0749a932
lifesinger <lifesinger@gmail.com>
2012-05-30 08:19:54 +0800

    fix jasmine alias bug

--- tools/seajs-and-its-friends.js
+++ tools/seajs-and-its-friends.js
@@ -48,10 +48,10 @@ seajs.config({
         /* placeholder 尚未开始开发 */
 
         // Widgets
-        'overlay': 'overlay/0.9.1/overlay',                  // 90%  完善阶段
-        'mask': 'overlay/0.9.1/mask',                        // 90%  完善阶段
-        'dialog': 'dialog/0.9.0/dialog',                     // 60%  讨论阶段
+        'overlay': 'overlay/0.9.3/overlay',                  // 90%  完善阶段
+        'mask': 'overlay/0.9.3/mask',                        // 90%  完善阶段
         'dropdown': 'dropdown/0.9.0/dropdown',               // 60%  讨论阶段
+        'dialog': 'dialog/0.9.0/dialog',                     // 60%  讨论阶段
         'switchable': 'switchable/0.8.1/switchable'          // 80%  讨论阶段
         /* calendar 开发中 */
         /* tip 尚未开始开发 */
