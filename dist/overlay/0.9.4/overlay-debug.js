c5d530a6a26d8ea81e47337cfb27187ed7f00cea
popomore <sakura9515@gmail.com>
2012-06-04 22:19:50 +0800

    先临时解决下，因为已经判断在隐藏的情况下处理，还原直接就隐藏了。 #106

--- dist/overlay/0.9.4/overlay-debug.js
+++ dist/overlay/0.9.4/overlay-debug.js
@@ -79,7 +79,8 @@ define("#overlay/0.9.4/overlay-debug", ["$","position","iframe-shim","widget"], 
 
             // 在定位时，为避免元素高度不定，先显示出来
             if (isHidden) {
-                this.element.css({ visibility: 'hidden', display: 'block' });
+                this.element.css({ visibility: 'hidden'});
+                this.set('visible', true);
             }
 
             Position.pin({
@@ -94,7 +95,8 @@ define("#overlay/0.9.4/overlay-debug", ["$","position","iframe-shim","widget"], 
 
             // 定位完成后，还原
             if (isHidden) {
-                this.element.css({ visibility: '', display: '' });
+                this.element.css({ visibility: ''});
+                this.set('visible', false);
             }
 
             return this;
