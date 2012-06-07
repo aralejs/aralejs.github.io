cf279793c941395876889fbc60d0b212c719e95c
lifesinger <lifesinger@gmail.com>
2012-06-07 09:53:13 +0800

    add StaticsFilter feature for class module

--- tools/seajs-and-its-friends.js
+++ tools/seajs-and-its-friends.js
@@ -35,7 +35,7 @@ seajs.config({
         // Infrastructure
         /* seajs 1.2 尚未开始 */
         'events': 'events/0.9.1/events',                     // 100%
-        'class': 'class/0.9.1/class',                        // 100%
+        'class': 'class/0.9.2/class',                        // 100%
         'base': 'base/0.9.14/base',                          // 95%
         'widget': 'widget/0.9.12/widget',                    // 95%
         'templatable': 'widget/0.9.12/templatable',          // 95%
