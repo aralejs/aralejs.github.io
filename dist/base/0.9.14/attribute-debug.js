d0d50f016d7af5480a79b4f10e9266236d16d7b2
lifesinger <lifesinger@gmail.com>
2012-06-07 17:04:51 +0800

    improve config merge logic

--- dist/base/0.9.14/attribute-debug.js
+++ dist/base/0.9.14/attribute-debug.js
@@ -13,7 +13,7 @@ define("#base/0.9.14/attribute-debug", [], function(require, exports) {
     exports.initAttrs = function(config, dataAttrsConfig) {
         // 合并来自 data-attr 的配置
         if (dataAttrsConfig) {
-            config = merge(dataAttrsConfig, config);
+            config = config ? merge(dataAttrsConfig, config) : dataAttrsConfig;
         }
 
         var specialProps = this.propsInAttrs || [];
