d4425c8a385aae0e8c8cbebd1e50e834eb53efd8
Shawn <shaoshuai0102@gmail.com>
2012-06-17 15:28:19 +0800

    add validator-0.8.1

---
+++ dist/validator/0.8.1/item-debug.js
@@ -0,0 +1,106 @@
+define("#validator/0.8.1/item-debug", ["$","./utils","widget","./async","./rule"], function(require, exports, module) {
+    var $ = require('$'),
+        utils = require('./utils'),
+        Widget = require('widget'),
+        async = require('./async'),
+        Rule = require('./rule');
+
+    var setterConfig = {
+        value: function() {},
+        setter: function(val) {
+            return typeof val != 'function' ? utils.helper(val) : val;
+        }
+    };
+    var Item = Widget.extend({
+        attrs: {
+            rule: '',
+            display: null,
+            triggerType: {
+                setter: function(val) {
+                    var element = $(this.get('element')),
+                        type = element.attr('type');
+                    
+                    var b = element.get(0).tagName.toLowerCase().indexOf('select') > -1 || type == 'radio' || type == 'checkbox';
+                    if (b && (val.indexOf('blur') > -1 || val.indexOf('key') > -1))
+                        return 'change';
+                    return val;
+                }
+            },
+            required: false,
+            checkNull: true,
+            onItemValidate: setterConfig,
+            onItemValidated: setterConfig,
+            showMessage: setterConfig,
+            hideMessage: setterConfig
+        },
+
+        setup: function() {
+            if (this.get('required')) {
+                if (!this.get('rule') || this.get('rule').indexOf('required') < 0) {
+                    this.set('rule', 'required ' + this.get('rule'));
+                }
+            }
+        },
+
+        execute: function(callback) {
+
+            this.trigger('itemValidate', this.element);
+
+            var rules = utils.parseRules(this.get('rule')),
+                that = this;
+
+            if (!rules) return this;
+
+            _metaValidate(this.element, rules, this.get('display'), function(err, msg) {
+                if (err) {
+                    var message = that.get('errormessage') || that.get('errormessage' + upperFirstLetter(err)) || msg;
+                } else {
+                    var message = msg;
+                }
+                that.trigger('itemValidated', that.element, err, message);
+                callback && callback(err, message, that.element);
+            });
+
+            return this;
+        }
+    });
+
+    function upperFirstLetter(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
+    function _metaValidate(ele, rules, display, callback) {
+
+        if (!$.isArray(rules))
+            throw new Error('No validation rule specified or not specified as an array.');
+        
+        var tasks = [];
+
+        $.each(rules, function(i, item) {
+
+            var obj = utils.parseRule(item),
+                ruleName = obj.name,
+                param = obj.param;
+
+            var rule = Rule.getOperator(ruleName);
+            if (!rule)
+                throw new Error('Validation rule with name "' + ruleName + '" cannot be found.');
+
+            var options = $.extend({}, param, {
+                element: ele,
+                display: (param && param.display) || display || $(ele).attr('name'),
+                rule: ruleName
+            });
+
+            tasks.push(function(cb) {
+                rule(options, cb);
+            });
+        });
+
+        async.series(tasks, function(err, results) {
+            callback && callback(err, results[results.length - 1]);
+        });
+    }
+
+    module.exports = Item;
+});
---
+++ dist/validator/0.8.1/validator-debug.js
@@ -0,0 +1,100 @@
+define("#validator/0.8.1/validator-debug", ["./core","widget","$"], function(require, exports, module) {
+    var Core = require('./core'),
+        Widget = require('widget'),
+        $ = require('$');
+
+    var Validator = Core.extend({
+
+        events: function() {
+            var hash = {};
+            hash['mouseenter .' + this.get('inputClass')] = 'mouseenter';
+            hash['mouseleave .' + this.get('inputClass')] = 'mouseleave';
+            hash['mouseenter .' + this.get('textareaClass')] = 'mouseenter';
+            hash['mouseleave .' + this.get('textareaClass')] = 'mouseleave';
+            hash['focus .' + this.get('itemClass')] = 'focus';
+            hash['blur .' + this.get('itemClass')] = 'blur';
+
+            return hash;
+        },
+
+        attrs: {
+            explainClass: 'ui-form-explain',
+            itemClass: 'ui-form-item',
+            itemHoverClass: 'ui-form-item-hover',
+            itemFocusClass: 'ui-form-item-focus',
+            itemErrorClass: 'ui-form-item-error',
+            inputClass: 'ui-input',
+            textareaClass: 'ui-textarea',
+
+            showMessage: function(element, message) {
+                this.getExplain(element).html(message);
+                this.getItem(element).addClass(this.get('itemErrorClass'));
+            },
+
+            hideMessage: function(element, message) {
+                this.getExplain(element).html(element.data('explain') || ' ');
+                this.getItem(element).removeClass(this.get('itemErrorClass'));
+            }
+        },
+
+        addItem: function(cfg) {
+            Validator.superclass.addItem.apply(this, [].slice.call(arguments));
+            var item = this.query(cfg.element);
+            this._saveExplainMessage(item);
+        },
+
+        _saveExplainMessage: function(item) {
+            var that = this;
+            var ele = item.element;
+
+            var explain = ele.data('explain');
+            // If explaining message is not specified, retrieve it from data-explain attribute of the target
+            // or from DOM element with class name of the value of explainClass attr.
+            // Explaining message cannot always retrieve from DOM element with class name of the value of explainClass
+            // attr because the initial state of form may contain error messages from server.
+            !explain && ele.data('explain', ele.attr('data-explain') || this.getExplain(ele).html());
+        },
+
+        getExplain: function(ele) {
+            var item = this.getItem(ele);
+            var explain = item.find('.' + this.get('explainClass'));
+
+            if (explain.length == 0) {
+                //console.log(1, '<div class="' + this.get('explainClass') + '"></div>', item);
+                var explain = $('<div class="' + this.get('explainClass') + '"></div>').appendTo(item);
+            }
+
+            //console.log('explain', explain);
+            return explain;
+        },
+
+        getItem: function(ele) {
+            ele = $(ele);
+            var item = ele.parents('.' + this.get('itemClass'));
+
+            return item;
+        },
+
+        mouseenter: function(e) {
+            this.getItem(e.target).addClass(this.get('itemHoverClass'));
+        },
+
+        mouseleave: function(e) {
+            this.getItem(e.target).removeClass(this.get('itemHoverClass'));
+        },
+
+        focus: function(e) {
+            var target = e.target;
+            this.getItem(target).removeClass(this.get('itemErrorClass'));
+            this.getItem(target).addClass(this.get('itemFocusClass'));
+            this.getExplain(target).html($(target).data('explain') || ' ');
+        },
+
+        blur: function(e) {
+            this.getItem(e.target).removeClass(this.get('itemFocusClass'));
+        }
+    });
+
+
+    module.exports = Validator;
+});
