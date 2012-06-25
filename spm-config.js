35a18420102bafe687c04c205a6db81a9ac91052
popomore <sakura9515@gmail.com>
2012-06-25 15:39:35 +0800

    公用 spm-config 只剩外部引入的模块

--- spm-config.js
+++ spm-config.js
@@ -1,9 +1,6 @@
 // arale alias 
 define({
-
     alias: {
-
-        // 外来模块
         '$': 'jquery/1.7.2/jquery',
         //'$': 'zepto/1.0.0/zepto',
         'jquery': 'jquery/1.7.2/jquery',
@@ -16,58 +13,7 @@ define({
         'store': 'store/1.3.3/store',
         'swfobject': 'swfobject/2.2.0/swfobject',
         'backbone': 'backbone/0.9.2/backbone',
-        'jasmine': 'jasmine/1.1.0/jasmine-html',
-
-
-        // Infrastructure
-        /* seajs 1.2 尚未开始 */
-        'events': 'events/0.9.1/events',                     // 100%
-        'class': 'class/0.9.2/class',                        // 100%
-        'base': 'base/0.9.15/base',                          // 95%
-        'widget': 'widget/0.9.15/widget',                    // 95%
-        'templatable': 'widget/0.9.15/templatable',          // 95%
-        'daparser': 'widget/0.9.15/daparser',                // 95%
-
-
-        // Utilities
-        'cookie': 'cookie/1.0.2/cookie',                     // 100%
-        'easing': 'easing/0.9.2/easing',                     // 100%
-        'iframe-shim': 'iframe-shim/0.9.1/iframe-shim',      // 95%
-        'position': 'position/0.9.1/position',               // 95%
-
-        'validator-core': 'validator/0.8.0/core.js',         // 60%
-        'validator': 'validator/0.8.0/validator.js',         // 60%
-        /* querystring 待迁移 */
-
-
-        // Widgets
-        'overlay': 'overlay/0.9.6/overlay',                  // 95%
-        'mask': 'overlay/0.9.6/mask',                        // 95%
-        'dialog': 'dialog/0.9.0/dialog',                     // 70%
-
-        'triggerable': 'triggerable/0.9.3/triggerable',      // 95%
-        'dropdown': 'triggerable/0.9.3/dropdown',            // 95%
-
-        'switchable': 'switchable/0.9.4/switchable',         // 90%
-        'tabs': 'switchable/0.9.4/tabs',                     // 90%
-        'slide': 'switchable/0.9.4/slide',                   // 90%
-        'accordion': 'switchable/0.9.4/accordion',           // 90%
-        'carousel': 'switchable/0.9.4/carousel',             // 90%
-
-        'calendar': 'calendar/0.8.0/calendar'                // 80%
-
-
-        // Others
-        /* spm */
-        /* araledoc */
-
-
-        // 二期组件
-        // ua
-        // placeholder
-        // popup
-        // tooltip
-        // 等待详细规划和讨论
+        'jasmine': 'jasmine/1.1.0/jasmine-html'
     },
     preload: [this.JSON ? '' : 'json', 'plugin-text']
 });
