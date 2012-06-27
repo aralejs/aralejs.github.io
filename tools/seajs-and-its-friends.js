2c2bd4ef20211527af2ca4a483995ad1e08352ae
lepture <lepture@me.com>
2012-06-27 17:16:23 +0800

    update seajs and its friends version

--- tools/seajs-and-its-friends.js
+++ tools/seajs-and-its-friends.js
@@ -38,10 +38,10 @@ seajs.config({
         /* seajs 1.2 尚未开始 */
         'events': 'events/0.9.1/events',                     // 100%
         'class': 'class/0.9.2/class',                        // 100%
-        'base': 'base/0.9.15/base',                          // 95%
-        'widget': 'widget/0.9.15/widget',                    // 95%
-        'templatable': 'widget/0.9.15/templatable',          // 95%
-        'daparser': 'widget/0.9.15/daparser',                // 95%
+        'base': 'base/0.9.16/base',                          // 95%
+        'widget': 'widget/0.9.16/widget',                    // 95%
+        'templatable': 'widget/0.9.16/templatable',          // 95%
+        'daparser': 'widget/0.9.16/daparser',                // 95%
 
 
         // Utilities
