35b87e5e5ee42728fb48ac137fe9215228d2450e
afc163 <afc163@gmail.com>
2013-06-28 19:46:05 +0800

    fix side highlight

--- static/side.js
+++ static/side.js
@@ -3,7 +3,7 @@ define(function(require, exports, module) {
     var $ = require('$');
     
     exports.init = function() {
-        var titles = $('.content-area > .content > h2, h2.alice-module-title');
+        var titles = $('.content-area > .content > h2, #module-wrapper > h2');
         var doc = $(document);
         if (titles.length <= 0) {
             return;
